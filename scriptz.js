13b14f3876f57753b9c72c7944340e821f47b1e4
Damir Harambasic <damirhara@gmail.com>
2015-01-26 21:46:04 -0700

    updates

--- scriptz.js
+++ scriptz.js
@@ -1,42 +1,40 @@
-/////////////////////////////
 // scriptz.js by @damdeez ///
-/////////////////////////////
 
+// Auto resize #top section to fit users browser 
 
-// Auto resize top_content section to fit users browser 
+var autoResizeSection = function() {
+    document.getElementById("top").style.height = window.innerHeight + "px";
+    }
+    window.onresize = autoResizeSection;
+autoResizeSection(); 
 
-    function autoResizeSection() {
-        document.getElementById("top").style.height = window.innerHeight +"px";
-        }
-        window.onresize = autoResizeSection;
-            autoResizeSection();   
+/* $(function() {
+    $("#top").height(window.innerHeight + "px");
+}); */
 
+setInterval(function() {
+        $("#arrowdown").toggleClass("animated bounce");
+    }, 2000);
 
 // jQuery goodness
 
-    $(document).ready(function() {
-        $(".click").click(function() {
-            var offset = "";
-            $("html, body").animate({
-                scrollTop: $("#portfolio").offset().top + offset}, 500);
-
-        });
-
-        $(this).scroll(function() {
-            if ($(this).scrollTop() > 1200) {
-                $("#backtop").fadeIn(800);
-            } else {
-                $("#backtop").fadeOut(800);
-            }
-        });
-
-        $("#backtop").click(function() {
-            $("html, body").animate( {
-                scrollTop: 0}, 500);
-        });
-
-        setInterval(function() {
-            $("#arrowdown").toggleClass("animated bounce");
-        }, 2000);
-
+$(function() {
+    $(".click").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#portfolio").offset().top}, 500);
     });
+    $("#backtop").hide();
+
+    $(this).scroll(function() {
+        if ($(this).scrollTop() > 1200) {
+            $("#backtop").css("display", "inline");
+        } else {
+            $("#backtop").css("display", "none");
+        }
+    });
+
+    $("#backtop").click(function() {
+        $("html, body").animate({scrollTop: 0}, 500);
+    });
+
+});
