5f049bba0788ef8c52f4f25977f98261475ff1b6
Damir Harambasic <damirhara@gmail.com>
2015-03-01 18:52:11 -0700

    updates 03012015

--- scriptz.js
+++ scriptz.js
@@ -1,35 +1,38 @@
-// scriptz.js by @damdeez ///
+// scriptz.js by @damdeez //
 
-// Auto resize #top section to fit users browser 
-
-var autoResizeSection = function() {
+/* var autoResizeSection = function() {
     document.getElementById("top").style.height = window.innerHeight + "px";
     }
     window.onresize = autoResizeSection;
-autoResizeSection(); 
+autoResizeSection(); */
 
-/* $(function() {
-    $("#top").height(window.innerHeight + "px");
-}); */
-
-setInterval(function() {
-        $("#arrowdown").toggleClass("animated bounce");
-    }, 2000);
-
-// jQuery goodness
+// Auto resize #top section to fit users browser
 
 $(function() {
+    $("#top").height(window.innerHeight + "px");
+});
+
+setInterval(function() {
+    $("#arrowdown").toggleClass("animated shake");
+    $("#next-example").toggleClass("animated pulse");
+}, 2000);
+
+$(document).ready(function() {
+    //$("#profile_pic").addClass("animated lightSpeedIn");
+    
     $(".click").click(function() {
         $("html, body").animate({
             scrollTop: $("#portfolio").offset().top}, 500);
     });
     $("#backtop").hide();
 
-    $(this).scroll(function() {
-        if ($(this).scrollTop() > 1200) {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1000) {
             $("#backtop").css("display", "inline");
+            $(".example-imgs").css("width", "55%");
         } else {
             $("#backtop").css("display", "none");
+            $(".example-imgs").css("width", "50%");
         }
     });
 
@@ -37,4 +40,19 @@ $(function() {
         $("html, body").animate({scrollTop: 0}, 500);
     });
 
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=damdeez&api_key=f7c1211d729a780a97b6b279b82aaea6&limit=2&format=json&callback=?", function(data) {
+        var html = ""; // we declare the variable that we'll be using to store our information
+        var counter = 1; // we declare a counter variable to use with the if statement in order to limit the result to 1
+        $.each(data.recenttracks.track, function(i, item) {
+            if(counter == 1) {
+                html += "Currently listening to: <span><a href='" + item.url + "' target='_blank'>" + item.name + "</a> - " + item.artist["#text"] + "</span>";
+            } // close the if statement
+            counter++ // add 1 to the counter variable each time the each loop runs
+        }); // close each loop
+        $("#listening-to").append(html);
+    });
+    $("#typed").typed({
+        strings: ["^1000Damdeez.", "Dado.", "Damir.^5000"],
+        typeSpeed: 100
+    });
 });
